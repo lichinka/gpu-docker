fb88744cfc70fed47e2efbcca7bce70f90a545e5
Lucas Benedicic <lucasbe@greina24.cm.cluster>
2015-09-16 13:57:31 +0200

    Optionally choose the CUDA toolkit to create.-

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-echo "The container image will try to autodetect the NVIDIA driver"
-echo "Press any key to continue or Ctrl+C to exit ..."
-read -n 1
+TOOLKIT_VERSION="$1"
 
-docker build --rm -t cuda65 --file=Dockerfile.cuda65 .
+if [ -z "${TOOLKIT_VERSION}" ]; then
+    echo "$0 <version tag>"
+    echo
+    echo "where <version tag> should be one of:"
+    echo "	55	for CUDA 5.5"
+    echo "	65	for CUDA 6.5"
+    echo "	70	for CUDA 7.0"
+    exit 1
+else
+    echo "Ready to build a CUDA ${TOOLKIT_VERSION} image."
+    echo "Press any key to continue or Ctrl+C to exit ..."
+    read -n 1
+
+    docker build --rm -t cuda${TOOLKIT_VERSION} --file=Dockerfile.cuda${TOOLKIT_VERSION} .
+fi
--- start.sh
+++ start.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-#
-# get the driver version
-#
-DRIVER_VERSION="$( nvidia-smi | grep Driver | awk '{ print $3; }' )"
-if [ -z "${DRIVER_VERSION}" ]; then
-    echo "No CUDA driver could be detected"
+
+TOOLKIT_VERSION="$1"
+
+if [ -z "${TOOLKIT_VERSION}" ]; then
+    echo "$0 <version tag>"
+    echo
+    echo "where <version tag> should be one of:"
+    echo "	55	for CUDA 5.5"
+    echo "	65	for CUDA 6.5"
+    echo "	70	for CUDA 7.0"
     exit 1
 else
-    echo "CUDA driver ${DRIVER_VERSION} detected"
+    #
+    # get the driver version
+    #
+    DRIVER_VERSION="$( nvidia-smi | grep Driver | awk '{ print $3; }' )"
+    if [ -z "${DRIVER_VERSION}" ]; then
+        echo "No CUDA driver could be detected"
+        exit 1
+    else
+        echo "CUDA driver ${DRIVER_VERSION} detected"
+    fi
+
+    #
+    # get the driver files
+    #
+    CONT_DRIVER_FILES=""
+    HOST_DRIVER_FILES="$( ldconfig -p | grep 'libcuda.so' | grep 'lib64' | awk '{ print $4; }' )"
+    if [ -z "${HOST_DRIVER_FILES}" ]; then
+        HOST_DRIVER_FILES="$( ldconfig -p | grep 'libcuda.so' | grep -v 'lib32' | awk '{ print $4; }' )"
+    fi
+
+    for f in ${HOST_DRIVER_FILES}; do
+        CONT_DRIVER_FILES="${CONT_DRIVER_FILES} -v ${f}:/opt/cuda/lib64/$( basename ${f} )"
+    done
+    HOST_DIR_NAME="$( dirname ${f} )"
+    CONT_DRIVER_FILES="${CONT_DRIVER_FILES} -v ${HOST_DIR_NAME}/libcuda.so.${DRIVER_VERSION}:/opt/cuda/lib64/libcuda.so.${DRIVER_VERSION}"
+
+    docker run --rm                                                             \
+               -it                                                              \
+               --privileged                                                     \
+               ${CONT_DRIVER_FILES}                                             \
+               cuda${TOOLKIT_VERSION}:latest
 fi
 
-#
-# get the driver files
-#
-CONT_DRIVER_FILES=""
-HOST_DRIVER_FILES="$( ldconfig -p | grep 'libcuda.so' | grep -v 'lib32' | awk '{ print $4; }' )"
-
-for f in ${HOST_DRIVER_FILES}; do
-    CONT_DRIVER_FILES="${CONT_DRIVER_FILES} -v ${f}:/opt/cuda/lib64/$( basename ${f} )"
-done
-HOST_DIR_NAME="$( dirname ${f} )"
-CONT_DRIVER_FILES="${CONT_DRIVER_FILES} -v ${HOST_DIR_NAME}/libcuda.so.${DRIVER_VERSION}:/opt/cuda/lib64/libcuda.so.${DRIVER_VERSION}"
-
-echo "${CONT_DRIVER_FILES}"
-
-docker run --rm                                                             \
-           -it                                                              \
-           --privileged                                                     \
-           ${CONT_DRIVER_FILES}                                             \
-           cuda65:latest
